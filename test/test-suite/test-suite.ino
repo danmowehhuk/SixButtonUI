a553eee8ba37d36f838908754181d0ae07eaf7da
Dan Mowehhuk <danmowehhuk@gmail.com>
2025-02-25 17:26:05 -0800

    Working checkpoint after updating TestTool

--- test/test-suite/test-suite.ino
+++ test/test-suite/test-suite.ino
@@ -5,126 +5,62 @@
 
 #include "UIConfig.h"
 
+void after() {
+  // Return to initial state so memory matches
+  helper.reset();
+}
+
 void testNavConfigInitialized(TestInvocation* t) {
-  t->name = STR("Nav config initialization");
-  do {
-    if (strcmp(MODEL.titleLine, "Main Menu") != 0) {
-      t->message = STR("titleLine should have been 'Main Menu'");
-      break;
-    }
-    if (strcmp(MODEL.interactiveLine, "First") != 0) {
-      t->message = STR("interactiveLine should have been 'First'");
-      break;
-    }
-    t->success = true;
-  } while (false);
+  t->setName(F("Nav config initialization"));
+  t->assertEqual(MODEL.titleLine, F("Main Menu"), F("titleLine should have been 'Main Menu'"));
+  t->assertEqual(MODEL.interactiveLine, F("First"), F("interactiveLine should have been 'First'"));
 }
 
 void testMenuButtonAtRootLevel(TestInvocation* t) {
-  t->name = STR("Menu button toggles thru root menus");
-  do {
-    helper.pressAndReleaseMenuBack();
-    if (strcmp(MODEL.titleLine, "Settings") != 0) {
-      t->message = STR("titleLine should have been 'Settings'");
-      break;
-    }
-    helper.pressAndReleaseMenuBack();
-    if (strcmp(MODEL.titleLine, "Main Menu") != 0) {
-      t->message = STR("titleLine should have been 'Main Menu'");
-      break;
-    }
-    if (strcmp(MODEL.interactiveLine, "First") != 0) {
-      t->message = STR("interactiveLine should have been 'First'");
-      break;
-    }
-    t->success = true;
-  } while (false);
-  helper.reset();
+  t->setName(F("Menu button toggles thru root menus"));
+  helper.pressAndReleaseMenuBack();
+  t->assertEqual(MODEL.titleLine, F("Settings"), F("titleLine should have been 'Settings'"));
+  helper.pressAndReleaseMenuBack();
+  t->assertEqual(MODEL.titleLine, F("Main Menu"), F("titleLine should have been 'Main Menu'"));
+  t->assertEqual(MODEL.interactiveLine, F("First"), F("interactiveLine should have been 'First'"));
 }
 
 void testRootLevelSelectionPreserved(TestInvocation* t) {
-  t->name = STR("Returns to previous root menu selection");
-  do {
-    if (strcmp(MODEL.titleLine, "Main Menu") != 0) {
-      t->message = STR("need to start at 'Main Menu'");
-      break;
-    }
-    helper.pressAndReleaseMenuBack(); // Switch to 'Settings'
-    helper.pressAndReleaseDown(); // Select 'Date'
-    helper.pressAndReleaseMenuBack(); // Switch to 'Main Menu'
-    helper.pressAndReleaseMenuBack(); // Switch to 'Settings'
-    if (strcmp(MODEL.titleLine, "Settings") != 0) {
-      t->message = STR("should have returned to 'Settings'");
-      break;
-    }
-    if (strcmp(MODEL.interactiveLine, "Date") != 0) {
-      t->message = STR("'Date' should still be selected");
-      break;
-    }
-    t->success = true;
-  } while (false);
-  helper.reset();
+  t->setName(F("Returns to previous root menu selection"));
+  t->assertEqual(MODEL.titleLine, F("Main Menu"), F("need to start at 'Main Menu'"));
+  helper.pressAndReleaseMenuBack(); // Switch to 'Settings'
+  helper.pressAndReleaseDown(); // Select 'Date'
+  helper.pressAndReleaseMenuBack(); // Switch to 'Main Menu'
+  helper.pressAndReleaseMenuBack(); // Switch to 'Settings'
+  t->assertEqual(MODEL.titleLine, F("Settings"), F("should have returned to 'Settings'"));
+  t->assertEqual(MODEL.interactiveLine, F("Date"), F("'Date' should still be selected"));
 }
 
 void testEnterSelectInSubMenu(TestInvocation* t) {
-  t->name = STR("Submenu 'enter' goes to child");
-  do {
-    helper.pressAndReleaseDown();
-    helper.pressAndReleaseSelectEnter();
-    if (strcmp(MODEL.titleLine, "Second") != 0) {
-      t->message = STR("titleLine should have been 'Second'");
-      break;
-    }
-    t->success = true;
-  } while (false);
-  helper.reset();
+  t->setName(F("Submenu 'enter' goes to child"));
+  helper.pressAndReleaseDown();
+  helper.pressAndReleaseSelectEnter();
+  t->assertEqual(MODEL.titleLine, F("Second"), F("titleLine should have been 'Second'"));
 }
 
 void testBackToSubMenu(TestInvocation* t) {
-  t->name = STR("Returns to previous submenu selection");
-  do {
-    helper.pressAndReleaseDown();
-    char* selection = strdup(MODEL.interactiveLine);
-    helper.pressAndReleaseSelectEnter();
-    helper.pressAndReleaseMenuBack();
-    if (strcmp(MODEL.titleLine, "Main Menu") != 0) {
-      t->message = STR("should have returned to 'Main Menu'");
-      break;
-    }
-    if (strcmp(MODEL.interactiveLine, selection) != 0) {
-      t->message = STR("Previous selection should still be selected");
-      break;
-    }
-    helper.reset();
-    if (strcmp(MODEL.interactiveLine, "First") != 0) {
-      t->message = STR("Selection should have reset to 'First'");
-      break;
-    }
-    free(selection);
-    t->success = true;
-  } while (false);
+  t->setName(F("Returns to previous submenu selection"));
+  helper.pressAndReleaseDown();
+  char* selection = strdup(MODEL.interactiveLine);
+  helper.pressAndReleaseSelectEnter();
+  helper.pressAndReleaseMenuBack();
+  t->assertEqual(MODEL.titleLine, F("Main Menu"), F("should have returned to 'Main Menu'"));
+  t->assertEqual(MODEL.interactiveLine, selection, F("Previous selection should still be selected"));
+  free(selection);
 }
 
 void testSelectorCurrValue(TestInvocation* t) {
-  t->name = STR("Selector inits to currValue selection");
-  do {
-    if (strcmp(MODEL.interactiveLine, "First") != 0) {
-      t->message = STR("need to start with 'First' selected");
-      break;
-    }
-    helper.pressAndReleaseDown();
-    helper.pressAndReleaseSelectEnter();
-    if (strcmp(MODEL.titleLine, "Second") != 0) {
-      t->message = STR("Should have entered 'Second' selector");
-      break;
-    }
-    if (strcmp(MODEL.interactiveLine, "two") != 0) {
-      t->message = STR("Should have had selection 'two'");
-      break;
-    }
-    t->success = true;
-  } while (false);
-  helper.reset();
+  t->setName(F("Selector inits to currValue selection"));
+  t->assertEqual(MODEL.interactiveLine, F("First"), F("need to start with 'First' selected"));
+  helper.pressAndReleaseDown();
+  helper.pressAndReleaseSelectEnter();
+  t->assertEqual(MODEL.titleLine, F("Second"), F("Should have entered 'Second' selector"));
+  t->assertEqual(MODEL.interactiveLine, "two", F("Should have had selection 'two'"));
 }
 
 Eventuino evt;
@@ -146,9 +82,8 @@ void setup() {
     testSelectorCurrValue
 
   };
-  int numTests = 6;
 
-  runTestSuite(tests, numTests, true);
+  runTestSuiteShowMem(tests, nullptr, after);
 }
 
 void loop() {}
