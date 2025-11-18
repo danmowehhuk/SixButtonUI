f531168509c53415a935f56bcb3a8aabd9c8992e
Dan Mowehhuk <danmowehhuk@gmail.com>
2025-11-17 18:32:38 -0800

    SelectorWidget::onEnter handles ComboBox case

--- src/sixbuttonui/SelectorWidget.h
+++ src/sixbuttonui/SelectorWidget.h
@@ -2,6 +2,7 @@
 #define _sixbuttonui_SelectorWidget_h
 
 
+#include <Arduino.h>
 #include "SelectorModel.h"
 #include "Widget.h"
 
@@ -42,6 +43,35 @@ class SelectorWidget: public Widget {
             char* selectionName = m->_optionNames[m->_currIndex];
             bool isNamePmem = m->_isOptionNamePmem[m->_currIndex];
             bool isValuePmem = m->_isOptionValuePmem[m->_currIndex];
+            char* searchPrefix = m->getSearchPrefix();
+            if (searchPrefix && strlen(searchPrefix) > 0) {
+              // handle the ComboBox case - concatenate searchPrefix and selectionName in a static buffer
+              static char buffer[64]; // Enough for prefix + value + null terminator
+              uint8_t pos = 0;
+              // Copy searchPrefix (in RAM)
+              const char* src = searchPrefix;
+              while (pos < 63 && *src != '\0') {
+                buffer[pos++] = *src++;
+              }
+              // Append selectionValue (may be in PROGMEM)
+              if (isValuePmem) {
+                src = selectionName;
+                while (pos < 63) {
+                  char c = pgm_read_byte(src);
+                  if (c == '\0') break;
+                  buffer[pos++] = c;
+                  src++;
+                }
+              } else {
+                src = selectionName;
+                while (pos < 63 && *src != '\0') {
+                  buffer[pos++] = *src++;
+                }
+              }
+              buffer[pos] = '\0';
+              selectionName = buffer;
+              isValuePmem = false; // buffer is in RAM, not PROGMEM
+            }
             state = _config->onEnterFunc(selectionName, isNamePmem, selectionValue, isValuePmem, state);
           }
         }
